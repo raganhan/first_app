4eafdf548d2cc1879b173733d8561a8fd57a37f5
Fernando Raganahn Barbosa <fernandorb86@gmail.com>
2013-05-16 13:21:37 -0300

    finished static pages chapter, next exercises

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+  	it "should have the right title" do
+		  visit '/static_pages/home'
+		  page.should have_selector('title',
+		                    :text => "Rails Tutorial Twitter Clone | Home")
+		end
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Twitter Clone')
+    end
+  end
+
+  describe "Help page" do
+
+  	it "should have the right title" do
+		  visit '/static_pages/help'
+		  page.should have_selector('title',
+		                    :text => "Rails Tutorial Twitter Clone | Help")
+		end
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+  end
+
+  describe "About page" do
+
+  	it "should have the right title" do
+		  visit '/static_pages/about'
+		  page.should have_selector('title',
+		                    :text => "Rails Tutorial Twitter Clone | About")
+		end
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+  end
+end
